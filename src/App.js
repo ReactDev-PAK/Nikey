198e1cf85b752e3b95967012aabc3eb3dac9b34b
SYEDTAYYABSAGHEER <syedtayyabsagheer@gmail.com>
2023-10-17 09:27:22 +0500

    Import the files in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import Popular from "./sections/Popular";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
+
 const App = () => {
   <main className="relative">
     <section className="x1:padding-1 wide:padding-r padding-b">
